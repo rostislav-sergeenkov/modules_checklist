1a682892cededaeddcf75d2309aa9da9602f152c
Rostislav Sergeenkov <rostislav.sergeenkov@gmail.com>
2016-06-30 01:25:23 +0300

    status check was fixed and JS effect was updated

--- modules_checklist.js
+++ modules_checklist.js
@@ -6,10 +6,22 @@
 (function ($) {
   Drupal.behaviors.modulesChecklist = {
     attach: function() {
-      $('#modules-checklist-settings-form input#edit-check-all-enabled').click(function() {
-        $checkBoxes = $('#modules-checklist-settings-form input.enabled_module');
-        $checkBoxes.attr('checked', !$checkBoxes.attr('checked'));
-      })
+      /**
+       * Implements 'Check all enabled modules' logic.
+       */
+      $('#modules-checklist-settings-form input#edit-check-all-enabled').click(function(event) {
+        var checkedEnabledModules = $('#modules-checklist-settings-form input:checked.enabled_module');
+        var allEnabledModules = $('#modules-checklist-settings-form input.enabled_module');
+        var allDisabledModules = $('#modules-checklist-settings-form input.disabled_module');
+
+        if (checkedEnabledModules.length != allEnabledModules.length) {
+          allEnabledModules.attr('checked', 'checked');
+          allDisabledModules.removeAttr('checked');
+        }
+        else {
+          checkedEnabledModules.removeAttr('checked');
+        }
+      });
     }
   }
 })(jQuery);
